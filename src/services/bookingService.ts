c020e9c8d78b503ad1a0e4e33533e7acfd4f1c49
danipoav <dapoav2002@gmail.com>
2025-01-29 15:29:33 +0100

    Changing services from bookings

--- src/services/bookingService.ts
+++ src/services/bookingService.ts
@@ -4,27 +4,35 @@ import { v4 as uuidv4 } from 'uuid'
 import connection from "../database";
 
 export const fetchAllBookings = async () => {
-    const [rows]: any =  await connection.query('SELECT * FROM bookings')
+    const [rows]: any = await connection.query('SELECT * FROM bookings')
     return rows;
 };
 
 export const fetchBookingById = async (id: string) => {
-    return await Booking.findOne({ id })
+    const [rows]: any = await connection.query('SELECT * FROM bookings WHERE id = ?', [id]);
+    return rows.length ? rows[0] : null;
 };
 
 export const addBooking = async (data: BookingType) => {
-    const booking = { ...data, id: uuidv4() };
-    const newBooking = new Booking(booking);
-    await newBooking.save();
-    return await Booking.find();
+    const id = uuidv4();
+    const { name, photo, check_in, check_out, room, requests, booking_date, price, status } = data;
+    await connection.query(
+        'INSERT INTO bookings (id,name, photo, check_in, check_out,room,requests,booking_date,price,status) VALUES (?, ?, ?, ?, ?)',
+        [id, name, photo, check_in, check_out, room, requests, booking_date, price, status]
+    );
+    return fetchAllBookings();
 };
 
 export const editBooking = async (id: string, data: BookingTypeID) => {
-    await Booking.findOneAndUpdate({ id }, data, { new: true });
-    return Booking.find();
+    const { name, photo, check_in, check_out, room, requests, booking_date, price, status } = data;
+    await connection.query(
+        'UPDATE bookings SET name = ?, photo = ?, check_in = ?, check_out = ?,room = ?,requests = ?,booking_date = ?, price = ?,status = ?, WHERE id = ?',
+        [name, photo, check_in, check_out, room, requests, booking_date, price, status, id]
+    );
+    return fetchAllBookings();
 };
 
 export const removeBooking = async (id: string) => {
-    await Booking.findOneAndDelete({ id });
-    return await Booking.find();
+    await connection.query('DELETE FROM bookings WHERE id = ?', [id]);
+    return fetchAllBookings();
 };
